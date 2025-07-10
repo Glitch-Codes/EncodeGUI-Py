25af424de75022ab750eaa6dbf424d34c6c55487
Glitch <54895281+Glitch-Codes@users.noreply.github.com>
2025-07-10 15:44:28 -0230

    More refactoring and created initial py file

---
+++ EncodeGUI.py
@@ -0,0 +1,36 @@
+import sys
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QMenu
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("EncodeGUI-Py")
+
+        button = QPushButton("Press Me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
+    def contextMenuEvent(self, e):
+        context = QMenu(self)
+        context.addAction(QAction("test 1", self))
+        context.addAction(QAction("test 2", self))
+        context.addAction(QAction("test 3", self))
+        context.exec(e.globalPos())
+
+    def the_button_was_clicked(self):
+        print("Clicked!")
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
